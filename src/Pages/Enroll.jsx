c104084fe8ed9abb67541e9fb8c03982f8cd5259
ssbbullseye7 <164629538+ssbbullseye7@users.noreply.github.com>
2024-10-19 19:36:44 +0530

    Update Enroll.jsx

--- src/Pages/Enroll.jsx
+++ src/Pages/Enroll.jsx
@@ -180,7 +180,7 @@ function Enroll() {
 
     // Update placeholder based on the selected registration type
     switch (registration) {
-      case 'Discounted Batch of 15 Candidates(Discounted batch 04th Nov 2024)':
+      case 'Batch of 15 Candidates(Special Discount for course-04th Nov 2024)':
         setPlaceholder('10000');
         break;
       // case 'Batch 2 Registration ( 7-11 Candidates )':
@@ -460,7 +460,7 @@ function Enroll() {
               required
             >
               <option value="">Select Type</option>
-              <option value="Discounted Batch of 15 Candidates(Discounted batch 04th Nov 2024)">
+              <option value="Batch of 15 Candidates(Special Discount for course-04th Nov 2024)">
               Discounted Batch of 15 Candidates(Discounted batch 04th Nov 2024)
               </option>
               {/* <option value="Batch 2 Registration ( 7-11 Candidates )">
